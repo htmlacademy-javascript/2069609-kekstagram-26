00859949ab4422697027d3f96b9ff36e0263b334
delannushka <anna_delyan@mail.ru>
2022-06-06 14:57:52 +0300

    внесла правки

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
-function getRandomNumber (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+function getRandomNumber (a, b) {
   let randomNumber;
-
-  if (min < 0 || min > max) {
+  if (a < 0 || a > b) {
     randomNumber = null;
-    return randomNumber;
+    throw '!!Проверьте значения диапазона!!';
   }
-  else if (min === max) {
+
+  const min = Math.ceil(a);
+  const max = Math.floor(b);
+
+  if (min > max) {
+    randomNumber = null;
+    throw '!!Проверьте значения диапазона!!';
+  }
+  if (min === max) {
     randomNumber = min;
     return randomNumber;
   }
+
   randomNumber = Math.random() * (max - min + 1);
   return Math.floor(randomNumber) + min;
 }
 
 getRandomNumber(0.5,0.6);
 
-function isLengthValid (string, maxLength) {
-  if (string.length <= maxLength) {
-    return true;
+
+function isLengthValid (str, maxLength) {
+  if (typeof str !== 'string') {
+    throw '!!Проверяемое значениме не является строкой!!';
   }
-  return false;
+  return (str.length <= maxLength);
 }
 
-isLengthValid('Привет', 3);
+isLengthValid('Привет',3);
