a94ce07a41ab722f7fe7642cfa857a61139b6b54
delannushka <anna_delyan@mail.ru>
2022-06-12 17:39:45 +0300

    вносит правки

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function getRandomNumber (minRange, maxRange) {
   let randomNumber;
   if (minRange < 0 || minRange > maxRange) {
     randomNumber = null;
-    throw '!!Проверьте значения диапазона!!';
+    throw new RangeError('!!Проверьте значения диапазона!!');
   }
 
   const min = Math.ceil(minRange);
@@ -10,7 +10,7 @@ function getRandomNumber (minRange, maxRange) {
 
   if (min > max) {
     randomNumber = null;
-    throw '!!Проверьте значения диапазона!!';
+    throw new RangeError('!!Проверьте значения диапазона!!');
   }
   if (min === max) {
     randomNumber = min;
@@ -23,7 +23,7 @@ function getRandomNumber (minRange, maxRange) {
 
 function isLengthValid (str, maxLength) {
   if (typeof str !== 'string') {
-    throw '!!Проверяемое значениме не является строкой!!';
+    throw new RangeError('!!Проверяемое значениме не является строкой!!');
   }
   return str.length <= maxLength;
 }
@@ -58,15 +58,14 @@ const DESCRIPTIONS = [
   'Привет!'
 ];
 
-
 const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
+
 const NAMES = [
   'Иван',
   'Хуан Себастьян',
@@ -95,44 +94,39 @@ const NAMES = [
   'Тор'
 ];
 
-// Создаю массив  из 25-ти случайных неповторяющихся чисел в диапозоне (0; 100].
-const idComment = [];
-while (idComment.length < 25) {
-  const randomNumber = Math.ceil(Math.random() * 100);
-  let found = false;
-  for (let i = 0; i < idComment.length; i++) {
-    if (idComment[i] === randomNumber || idComment[i]===0){
-      found = true;
-      break;
-    }
-  }
-  if (!found) {
-    idComment[idComment.length]=randomNumber;
-  }
-}
+//Создаю массив до 1 до 100
+const arrayOfNumbers100 = Array.from({length: 100}, (v, i) => i + 1);
+//Перетасую элементы массива
+const IDCOMMENTS = arrayOfNumbers100.sort(() => Math.random() - 0.5);
 
 let j = 0;
-const COMMENTS = () => ({
-  id: idComment[j++],
+//Функция для создания объекта Комментарий
+const getComment = () => ({
+  id: IDCOMMENTS[j++],
   avatar: `img/avatar-${getRandomNumber (1, 6)}.svg`,
   message: MESSAGES[getRandomNumber (0, 5)],
   nameComment: NAMES[getRandomNumber (0, 24)],
 }
 );
 
-let photoIndex = 1;
-let urlIndex = 1;
-let descriptionIndex = 0;
-const createPhoto = () => ({
-  id: photoIndex++,
-  url: `photos/${urlIndex++}.jpg`,
-  description: DESCRIPTIONS[descriptionIndex++],
-  likes: getRandomNumber (15, 200),
-  comments: COMMENTS()
-}
-);
+const createPhotoMocks = (count) => {
+  let photoIndex = 1;
+  let urlIndex = 1;
+  let descriptionIndex = 0;
+  const photos = [];
+  while (photoIndex <= count) {
+    photos.push({
+      id: photoIndex++,
+      url: `photos/${urlIndex++}.jpg`,
+      description: DESCRIPTIONS[descriptionIndex++],
+      likes: getRandomNumber (15, 200),
+      comments: [getComment()]
+    });
+  }
+  return photos;
+};
 
-// eslint-disable-next-line no-unused-vars
-const similarPhotos = Array.from({length: 25}, createPhoto);
-
-//console.log(similarPhotos);
+const countPhotos = 25;
+const similarPhotos = createPhotoMocks(countPhotos);
+// eslint-disable-next-line no-console
+console.log(similarPhotos);
